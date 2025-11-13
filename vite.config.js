50d0c59747311a96da5db2525f0364ef9556ec64
dkolkovskiy <160935515+dkolkovskiy@users.noreply.github.com>
2025-11-14 01:04:46 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,8 @@ const path = fileURLToPath(import.meta.url);
 export default {
   root: join(dirname(path), "client"),
   plugins: [react()],
+  server: {
+  allowedHosts: true,
+  host: true,
+},
 };
